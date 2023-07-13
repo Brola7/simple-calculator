bceb3251fe8a24f0ece24dcbcd935e516d0a9cdb
Rati Broladze <ratibroladze2004@gmail.com>
2023-07-13 20:40:11 +0400

    finish project

--- script.js
+++ script.js
@@ -24,160 +24,178 @@ let num1 = 0, num2 = 0, operator = "0";
 screen.textContent = a;
 one.addEventListener("click", ()=> {
     check(bool);
-    if(a !== "0"){
-        b = "1"
-        a = a.concat(b);
-        screen.textContent = a;
-    }
-    else{
-    a = "1";
-    screen.textContent = a;
-    }
-    if(operator === "0"){
-        num1 = parseFloat(a);
-        }else{
-            num2 = parseFloat(a);
+    if(a.length < 9){
+        if(a !== "0"){
+            b = "1"
+            a = a.concat(b);
+            screen.textContent = a;
         }
+        else{
+        a = "1";
+        screen.textContent = a;
+        }
+        if(operator === "0"){
+            num1 = parseFloat(a);
+            }else{
+                num2 = parseFloat(a);
+            }
+    }
 })
 two.addEventListener("click", ()=> {
     check(bool);
-    if(a !== "0"){
-        b = "2"
-        a = a.concat(b);
-        screen.textContent = a;
-    }
-    else{
-    a = "2";
-    screen.textContent = a;
-    }
-    if(operator === "0"){
-        num1 = parseFloat(a);
-        }else{
-            num2 = parseFloat(a);
+    if(a.length < 9){
+        if(a !== "0"){
+            b = "2"
+            a = a.concat(b);
+            screen.textContent = a;
         }
+        else{
+        a = "2";
+        screen.textContent = a;
+        }
+        if(operator === "0"){
+            num1 = parseFloat(a);
+            }else{
+                num2 = parseFloat(a);
+            }
+    }
 })
 three.addEventListener("click", ()=> {
     check(bool);
-    if(a !== "0"){
-        b = "3"
-        a = a.concat(b);
-        screen.textContent = a;
-    }
-    else{
-    a = "3";
-    screen.textContent = a;
-    }
-    if(operator === "0"){
-        num1 = parseFloat(a);
-        }else{
-            num2 = parseFloat(a);
+        if(a.length < 9){
+        if(a !== "0"){
+            b = "3"
+            a = a.concat(b);
+            screen.textContent = a;
         }
+        else{
+        a = "3";
+        screen.textContent = a;
+        }
+        if(operator === "0"){
+            num1 = parseFloat(a);
+            }else{
+                num2 = parseFloat(a);
+            }
+    }
 })
 four.addEventListener("click", ()=> {
     check(bool);
-    if(a !== "0"){
-        b = "4"
-        a = a.concat(b);
-        screen.textContent = a;
-    }
-    else{
-    a = "4";
-    screen.textContent = a;
-    }
-    if(operator === "0"){
-        num1 = parseFloat(a);
-        }else{
-            num2 = parseFloat(a);
+    if(a.length < 9){
+        if(a !== "0"){
+            b = "4"
+            a = a.concat(b);
+            screen.textContent = a;
         }
+        else{
+        a = "4";
+        screen.textContent = a;
+        }
+        if(operator === "0"){
+            num1 = parseFloat(a);
+            }else{
+                num2 = parseFloat(a);
+            }
+    }
 })
 five.addEventListener("click", ()=> {
     check(bool);
-    if(a !== "0"){
-        b = "5"
-        a = a.concat(b);
-        screen.textContent = a;
-    }
-    else{
-    a = "5";
-    screen.textContent = a;
-    }
-    if(operator === "0"){
-        num1 = parseFloat(a);
-        }else{
-            num2 = parseFloat(a);
+    if(a.length < 9){
+        if(a !== "0"){
+            b = "5"
+            a = a.concat(b);
+            screen.textContent = a;
         }
+        else{
+        a = "5";
+        screen.textContent = a;
+        }
+        if(operator === "0"){
+            num1 = parseFloat(a);
+            }else{
+                num2 = parseFloat(a);
+            }
+    }
 })
 six.addEventListener("click", ()=> {
     check(bool);
-    if(a !== "0"){
-        b = "6"
-        a = a.concat(b);
-        screen.textContent = a;
-    }
-    else{
-    a = "6";
-    screen.textContent = a;
-    }
-    if(operator === "0"){
-        num1 = parseFloat(a);
-        }else{
-            num2 = parseFloat(a);
+    if(a.length < 9){
+        if(a !== "0"){
+            b = "6"
+            a = a.concat(b);
+            screen.textContent = a;
         }
+        else{
+        a = "6";
+        screen.textContent = a;
+        }
+        if(operator === "0"){
+            num1 = parseFloat(a);
+            }else{
+                num2 = parseFloat(a);
+            }
+    }
 })
 seven.addEventListener("click", ()=> {
     check(bool);
-    if(a !== "0"){
-        b = "7"
-        a = a.concat(b);
-        screen.textContent = a;
-    }
-    else{
-    a = "7";
-    screen.textContent = a;
-    }
-    if(operator === "0"){
-        num1 = parseFloat(a);
-        }else{
-            num2 = parseFloat(a);
+    if(a.length < 9){
+        if(a !== "0"){
+            b = "7"
+            a = a.concat(b);
+            screen.textContent = a;
         }
+        else{
+        a = "7";
+        screen.textContent = a;
+        }
+        if(operator === "0"){
+            num1 = parseFloat(a);
+            }else{
+                num2 = parseFloat(a);
+            }
+    }
 })
 eight.addEventListener("click", ()=> {
     check(bool);
-    if(a !== "0"){
-        b = "8"
-        a = a.concat(b);
-        screen.textContent = a;
-    }
-    else{
-    a = "8";
-    screen.textContent = a;
-    }
-    if(operator === "0"){
-        num1 = parseFloat(a);
-        }else{
-            num2 = parseFloat(a);
+    if(a.length < 9){
+        if(a !== "0"){
+            b = "8"
+            a = a.concat(b);
+            screen.textContent = a;
         }
+        else{
+        a = "8";
+        screen.textContent = a;
+        }
+        if(operator === "0"){
+            num1 = parseFloat(a);
+            }else{
+                num2 = parseFloat(a);
+            }
+    }
 })
 nine.addEventListener("click", ()=> {
     check(bool);
-    if(a !== "0"){
-        b = "9"
-        a = a.concat(b);
-        screen.textContent = a;
-    }
-    else{
-    a = "9";
-    screen.textContent = a;
-    }
-    if(operator === "0"){
-        num1 = parseFloat(a);
-        }else{
-            num2 = parseFloat(a);
+    if(a.length < 9){
+        if(a !== "0" && a.length < 10){
+            b = "9"
+            a = a.concat(b);
+            screen.textContent = a;
         }
+        else{
+        a = "9";
+        screen.textContent = a;
+        }
+        if(operator === "0"){
+            num1 = parseFloat(a);
+            }else{
+                num2 = parseFloat(a);
+            }
+    }
 })
 zero.addEventListener("click", ()=> {
     check(bool);
-    if(a !== "0"){
+    if(a !== "0" && a.length < 10){
         b = "0"
         a = a.concat(b);
         screen.textContent = a;
@@ -242,7 +260,11 @@ equals.addEventListener("click", () => {
         }else {
             num1 = num1 / num2;
         }
+        if(num1.toString().length > 9){
+            a = num1.toString().slice(0,8);
+        }else{
         a = num1.toString();
+        }
         screen.textContent = a;
         num2 = 0;
         operator = "0";
